30ee78692e86768bdbf92798593bacddd4eab0fb
SlavaSaniuk <saniuk.vyacheslav.97@gmail.com>
2020-03-23 23:59:53 +0300

    Added Thymeleaf integration

--- src/main/java/by/bsac/Main.java
+++ src/main/java/by/bsac/Main.java
@@ -6,11 +6,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @Import({RootConfiguration.class, WebmvcConfiguration.class})
 public class Main extends SpringBootServletInitializer {
 
--- src/main/java/by/bsac/webmvc/WebmvcConfiguration.java
+++ src/main/java/by/bsac/webmvc/WebmvcConfiguration.java
@@ -2,9 +2,17 @@ package by.bsac.webmvc;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import static by.bsac.core.logging.SpringCommonLogging.*;
 
@@ -20,6 +28,54 @@ public class WebmvcConfiguration implements WebMvcConfigurer {
     public WebmvcConfiguration() {
         LOGGER.info(INITIALIZATION.startInitializeConfiguration(WebmvcConfiguration.class));
     }
+
+    /**
+     * Thymeleaf {@link org.thymeleaf.templateresolver.ITemplateResolver} implementation. Resolver configured
+     * to get html template's from "src/main/resources/html" directory.
+     * @return - configured {@link org.thymeleaf.templateresolver.ITemplateResolver} bean.
+     */
+    @Bean("TemplateResolver")
+    @Order(1)
+    public ITemplateResolver getClassLoaderTemplateResolver() {
+
+        LOGGER.info(CREATION.startCreateBean(BeanDefinition.of(ClassLoaderTemplateResolver.class)));
+        ClassLoaderTemplateResolver tr = new ClassLoaderTemplateResolver();
+
+        tr.setPrefix("html/");
+        tr.setSuffix(".html");
+
+        tr.setCacheable(false);
+        tr.setCharacterEncoding("UTF-8");
+        tr.setTemplateMode(TemplateMode.HTML);
+
+        LOGGER.info(CREATION.endCreateBean(BeanDefinition.of(ClassLoaderTemplateResolver.class)));
+        return tr;
+    }
+
+    @Bean
+    public ViewResolver getViewResolver() {
+        ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+
+        resolver.setTemplateEngine(this.getSpringTemplateEngine());
+        resolver.setOrder(1);
+
+        return resolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine getSpringTemplateEngine() {
+
+        LOGGER.info(CREATION.startCreateBean(BeanDefinition.of(SpringTemplateEngine.class)));
+        SpringTemplateEngine engine = new SpringTemplateEngine();
+
+        engine.setTemplateResolver(this.getClassLoaderTemplateResolver());
+        engine.setEnableSpringELCompiler(true);
+
+        LOGGER.info(CREATION.endCreateBean(BeanDefinition.of(SpringTemplateEngine.class)));
+        return engine;
+    }
+
+
 
 
 
